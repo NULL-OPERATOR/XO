44fce29651d2ea94a86bb407176417e23738f500
NULL-OPERATOR <x@cryptolab.net>
2016-02-15 21:11:31 +0000

    add start_game, and set players to game_setup

--- lib/game_setup.rb
+++ lib/game_setup.rb
@@ -1,11 +1,16 @@
 class GameSetup
   attr_reader :p1, :p2
+  def initialize(game, grid)
+    @game = game
+    @grid = grid
+  end
+
   def set_players(p1, p2)
     @p1 = p1
     @p2 = p2
   end
 
-  def start_game(grid)
-    Game.new(grid, @p1, @p2)
+  def start_game
+    @game.new(@grid, @p1, @p2)
   end
 end
--- spec/game_setup_spec.rb
+++ spec/game_setup_spec.rb
@@ -2,27 +2,18 @@ require "spec_helper"
 require "game_setup"
 
 describe GameSetup do
-  let(:setup)  { described_class.new }
-  let(:player) { double(:player) }
+  let(:grid)   { class_double("Grid") }
+  let(:game)   { spy("Game") }
+  let(:setup)  { described_class.new(game, grid) }
+  let(:player) { instance_double("player") }
 
   it("should be able to set players") do
     setup.set_players(player, player)
     expect(setup.p1).to eq(player)
   end
+
+  it("should initialize a game") do
+    setup.start_game
+    expect(game).to have_received(:new)
+  end
 end
-
-
-# describe GameSetup do
-#   let(:setup) { class_double(:GameSetup) }
-#   let(:grid) { class_double(:grid) }
-#
-#   it("should be able to set a player with choice")
-#
-#
-#   it("should return game with 0, when 2 humans") do
-#     mode = [true, true]
-#     let(:game) { Game.new(game, mode, grid) }
-#     expect
-#   end
-#       # expect(game.player2).to be("human")
-# end
